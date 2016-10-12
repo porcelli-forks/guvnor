07f6e16a66c9d38bd84b3804170f7aa249b5c3b3
salaboy <salaboy@gmail.com>
2016-10-12 14:21:39 +0100

    adding codeserver watcher and MavenProject Loader test

---
+++ guvnor-ala/guvnor-ala-build-maven/src/test/java/org/guvnor/ala/build/maven/executor/MavenProjectLoaderTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.guvnor.ala.build.maven.executor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.project.MavenProject;
+
+import org.guvnor.ala.source.Source;
+import org.guvnor.ala.source.git.GitHub;
+import org.guvnor.ala.source.git.GitRepository;
+import org.junit.After;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.scanner.embedder.MavenProjectLoader;
+import org.uberfire.java.nio.file.Files;
+
+/**
+ *
+ */
+public class MavenProjectLoaderTest {
+
+    private File tempPath;
+
+    @Before
+    public void setUp() throws IOException {
+        tempPath = java.nio.file.Files.createTempDirectory( "ccc" ).toFile();
+    }
+
+    @After
+    public void tearDown() {
+        FileUtils.deleteQuietly( tempPath );
+    }
+
+    @Test
+    public void helloMavenProjectLoader() {
+        final GitHub gitHub = new GitHub();
+        final GitRepository repository = ( GitRepository ) gitHub.getRepository( "salaboy/drools-workshop", new HashMap<String, String>() {
+            {
+                put( "out-dir", tempPath.getAbsolutePath() );
+            }
+        } );
+        final Source source = repository.getSource( "master" );
+        assertNotNull( source );
+        InputStream pomStream = Files.newInputStream( source.getPath().resolve( "drools-webapp-example" ).resolve( "pom.xml" ) );
+//        String result = getStringFromInputStream( pomStream );
+//
+//        System.out.println( result );
+//        System.out.println( "Done" );
+        MavenProject project = MavenProjectLoader.parseMavenPom( pomStream );
+
+        assertNotNull( project );
+        System.out.println( "Project " + project );
+        pomStream = Files.newInputStream( source.getPath().resolve( "drools-webapp-example" ).resolve( "pom.xml" ) );
+//        result = getStringFromInputStream( pomStream );
+        project = MavenProjectLoader.parseMavenPom( pomStream );
+
+        assertNotNull( project );
+        System.out.println( "Project " + project );
+// System.out.println( result );
+//        System.out.println( "Done" );
+
+    }
+
+    // convert InputStream to String
+    private static String getStringFromInputStream( InputStream is ) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader( new InputStreamReader( is ) );
+            while ( ( line = br.readLine() ) != null ) {
+                sb.append( line );
+            }
+
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+            if ( br != null ) {
+                try {
+                    br.close();
+                } catch ( IOException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+}
